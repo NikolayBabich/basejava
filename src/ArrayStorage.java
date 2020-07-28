15348ef49c26ee76f60cd2cb80090bc1f589ab99
glukoman <glukoman86@gmail.com>
2020-07-29 01:02:41 +0300

    Implemented save, get, delete, size, clear, getAll methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,64 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
+    private int nextValidId = 0;
 
+    /**
+     * Removes all of the Resumes from this storage, all elements of array are set to null
+     */
     void clear() {
-    }
-
-    void save(Resume r) {
-    }
-
-    Resume get(String uuid) {
-        return null;
-    }
-
-    void delete(String uuid) {
+        Arrays.fill(storage, null);
+        nextValidId = 0;
     }
 
     /**
-     * @return array, contains only Resumes in storage (without null)
+     * @param r Resume to be saved to this storage
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    void save(Resume r) {
+        storage[nextValidId] = r;
+        nextValidId++;
     }
 
+    /**
+     * @param uuid index of Resume to be returned, if present
+     * @return Resume with given uuid, or null if none
+     */
+    Resume get(String uuid) {
+        for (int i = 0; i < nextValidId; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param uuid index of Resume to be deleted from this storage, if present
+     */
+    void delete(String uuid) {
+        for (int i = 0; i < nextValidId; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = storage[nextValidId - 1];
+                storage[--nextValidId] = null;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in this storage (without null)
+     */
+    Resume[] getAll() {
+        return Arrays.copyOf(storage, nextValidId);
+    }
+
+    /**
+     * @return the number of Resumes in this storage
+     */
     int size() {
-        return 0;
+        return nextValidId;
     }
 }
